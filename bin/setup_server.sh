d071ce3be7abf11b55888fe866b438140f96b7a3
Wiley Kestner <wiley@aya.yale.edu>
2014-01-23 08:15:33 -0500

    Fixed setup script

--- bin/setup_server.sh
+++ bin/setup_server.sh
@@ -3,7 +3,7 @@
 cd Externals/django-rest-framework-api-server &&
 
 if [ ! -d venv ]; then
-virtualenv venv --distribute &&
+virtualenv venv --distribute
 fi
 
 source venv/bin/activate &&
@@ -11,10 +11,10 @@ pip install -r requirements.txt &&
 pip install -r requirements-dev.txt &&
 
 if [ ! -f .env ]; then
-    touch .env &&
-    echo "export LOCAL_DEV=1" >> .env &&
-    echo "export ALLOWED_HOSTS='localhost,127.0.0.1'" >> .env &&
-    echo "export SECRET_KEY='secret'" >> .env &&
+    touch .env
+    echo "export LOCAL_DEV=1" >> .env
+    echo "export ALLOWED_HOSTS='localhost,127.0.0.1'" >> .env
+    echo "export SECRET_KEY='secret'" >> .env
 fi
 
 source .env &&
